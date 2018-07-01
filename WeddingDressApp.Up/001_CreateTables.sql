152d0b52b1abdca3cdddc42211368424cbf8c4c7
Toni Sefton <21288605+Ariadne-S@users.noreply.github.com>
2018-07-01 14:37:15 +1000

    Added Clustered Indexes

--- WeddingDressApp.Up/001_CreateTables.sql
+++ WeddingDressApp.Up/001_CreateTables.sql
@@ -1,63 +1,69 @@
 ﻿CREATE TABLE Dresses (
-	DressId int PRIMARY KEY NOT NULL,
-	SequentialId int identity NOT NULL,
-    DressName nvarchar(25) NOT NULL,  
-	DressWebpage nvarchar (500),
+	DressId UniqueIdentifier PRIMARY KEY NONCLUSTERED NOT NULL,
+	SequentialId Int identity NOT NULL,
+    DressName nVarChar(25) NOT NULL,  
+	DressWebpage nVarChar (500),
     Price money,  
-    ProductDescription nvarchar (max),
-	DressType varchar (11) NOT NULL CHECK (DressType IN('Bride', 'Bridesmaids')),
-	RecommendedBy uniqueidentifier,
-	Approval varchar (8) NOT NULL CHECK (Approval IN('Yes', 'No', 'Required')),
-	Rating int,
-	ShopId uniqueidentifier,
-	ImageId uniqueidentifier
+    ProductDescription nVarChar (max),
+	DressType VarChar (11) NOT NULL, --CHECK (DressType IN('Bride', 'Bridesmaids')),
+	RecommendedBy UniqueIdentifier,
+	Approval VarChar (8) NOT NULL, --CHECK (Approval IN('Yes', 'No', 'Required')),
+	Rating Int,
+	ShopId UniqueIdentifier,
+	ImageId UniqueIdentifier
 )
+CREATE CLUSTERED INDEX DressesSeqId ON dbo.Dresses(SequentialId)
 
 CREATE TABLE Shops (
-	ShopId int PRIMARY KEY NOT NULL,
-	SequentialId int identity NOT NULL,
-    ShopName nvarchar(25) NOT NULL,  
-	ShopWebpage nvarchar (500),
-	ShopLocation nvarchar (30),
-	Logo varbinary(max) 
+	ShopId UniqueIdentifier PRIMARY KEY NONCLUSTERED NOT NULL,
+	SequentialId Int identity NOT NULL,
+    ShopName nVarChar(25) NOT NULL,  
+	ShopWebpage nVarChar (500),
+	ShopLocation nVarChar (30),
+	ImageId UniqueIdentifier NOT NULL
 )
+CREATE CLUSTERED INDEX ShopsSeqId ON dbo.Shops(SequentialId)
 
-CREATE TABLE JoinImage (
-	ImageJoinId int PRIMARY KEY NOT NULL,
-	SequentialId int identity NOT NULL,
-    DressId int NOT NULL,  
-	ImageId int NOT NULL,
+CREATE TABLE DressImages (
+	--ImageJoinId UniqueIdentifier PRIMARY KEY NONCLUSTERED NOT NULL,
+	SequentialId Int identity Primary Key NOT NULL,
+    DressId UniqueIdentifier NOT NULL,  
+	ImageId UniqueIdentifier NOT NULL,
 	Favourite bit NOT NULL
 )
 
 CREATE TABLE Images (
-	ImageId int PRIMARY KEY NOT NULL,
-	SequentialId int identity NOT NULL,
-    FileName varchar(15) NOT NULL,  
-	FileExtension varchar (5) NOT NULL,
-	FileData varbinary (max),
-	Hash var256
+	ImageId UniqueIdentifier PRIMARY KEY NONCLUSTERED NOT NULL,
+	SequentialId Int identity NOT NULL,
+    [FileName] VarChar(128) NOT NULL,  
+	FileExtension VarChar (32) NOT NULL,
+	FileData VarBinary (max),
+	[Hash] VarChar(256)
 )
+CREATE CLUSTERED INDEX ImagesSeqId ON dbo.Images(SequentialId)
 
 CREATE TABLE Users (
-	UserId int PRIMARY KEY NOT NULL,
-	SequentialId int identity NOT NULL,
-    Username varchar (20) NOT NULL,  
-	Email varchar (150) NOT NULL,
+	UserId UniqueIdentifier PRIMARY KEY NONCLUSTERED NOT NULL,
+	SequentialId Int identity NOT NULL,
+    Username VarChar (64) NOT NULL,  
+	Email VarChar (150) NOT NULL,
 	IsEmailValidated bit NOT NULL
 )
+CREATE CLUSTERED INDEX UsersSeqId ON dbo.Users(SequentialId)
 
 CREATE TABLE Weddings (
-	WeddingId int PRIMARY KEY NOT NULL,
-	SequentialId int identity NOT NULL,
-    Eventname varchar (20) NOT NULL,  
-	EventDate varchar (150) NOT NULL
+	WeddingId UniqueIdentifier PRIMARY KEY NONCLUSTERED NOT NULL,
+	SequentialId Int identity NOT NULL,
+    EventName VarChar (64) NOT NULL,  
+	EventDate DateTime2 NOT NULL
 )
+CREATE CLUSTERED INDEX WeddingsSeqId ON dbo.Weddings(SequentialId)
 
 CREATE TABLE Guests (
-	GuestId int PRIMARY KEY NOT NULL,
-	SequentialId int identity NOT NULL,
-    UserId int NOT NULL,  
-	WeddingId int NOT NULL,
-	GuestType varchar (20)
+	GuestId UniqueIdentifier PRIMARY KEY NONCLUSTERED NOT NULL,
+	SequentialId Int identity NOT NULL,
+    UserId UniqueIdentifier NOT NULL,  
+	WeddingId UniqueIdentifier NOT NULL,
+	GuestType VarChar (20)
 )
+CREATE CLUSTERED INDEX GuestsSeqId ON dbo.Guests(SequentialId)
